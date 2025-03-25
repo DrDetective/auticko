86f6f1cc60f66007f9d7178c626c0608879c42cb
Štěpán Bast <bastici30@gmail.com>
2025-03-25 10:43:04 +0100

    Přidejte soubory projektu.

---
+++ auticko/Metody.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace auticko
+{
+    internal class Metody
+    {
+        private float palivo;
+        public void Natankuj(int litr)
+        {
+            palivo += litr;
+        }
+        public void stavPaliva()
+        {
+            Console.WriteLine($"momentálně máš {palivo} litrů paliva");
+            Thread.Sleep(2000);
+        }
+        public void Jed()
+        {
+            Console.WriteLine("Kolik km chceš ujet?");
+            int km = Convert.ToInt32(Console.ReadLine());
+            float spotreba = (km/palivo)*100;
+            palivo -= spotreba;
+            if(palivo < 0)
+            {
+                Console.WriteLine("Není možné ujet tolik km s tímto množstím palivem");
+                Thread.Sleep(2000);
+            }
+            else
+            {
+                Console.WriteLine($"Spotřeboval jsi {spotreba} paliva na {km} km");
+                Thread.Sleep(2000);
+            }
+            //Debug.Write(spotreba);
+            //Debug.Write(palivo);
+        }
+    }
+}
---
+++ auticko/Program.cs
@@ -0,0 +1,30 @@
+﻿using auticko;
+using Spectre.Console;
+using System.Collections.Concurrent;
+
+Metody nevim = new Metody();
+bool exitBool = true;
+while (exitBool)
+{
+    var menu = AnsiConsole.Prompt(new SelectionPrompt<string>().PageSize(4).AddChoices(new[] { "Natankovat", "Stav paliva", "Projet se", "Exit" }));
+    switch (menu)
+    {
+        case "Natankovat":
+            Console.WriteLine("Zadej počet litrů kolik potřebuješ natankovat");
+            int litr = Convert.ToInt32(Console.ReadLine());
+            nevim.Natankuj(litr);
+            Console.Clear();
+            break;
+        case "Stav paliva":
+            nevim.stavPaliva();
+            Console.Clear();
+            break;
+        case "Projet se":
+            nevim.Jed();
+            Console.Clear();
+            break;
+        case "Exit":
+            exitBool = false;
+            break;
+    }
+}
